825449d3774089b97f778834fd1e0ce2d2fc53b6
Thea <thea.joy@outlook.com>
2025-04-16 00:58:57 +0200

    feat(layout): setting up layout

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -13,18 +13,16 @@
         </div>
         <div class="relative mt-6 flex items-center justify-center gap-x-6 sm:gap-x-8">
 
-            <button type="button" class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                <svg class="-ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" />
-                </svg>
-                Button text
+            <button type="button"
+                    class="uppercase inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <x-ri-bluesky-fill class="-ml-0.5 size-5"/>
+                BlueSky
             </button>
 
-            <button type="button" class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                <svg class="-ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" />
-                </svg>
-                Button text
+            <button type="button"
+                    class="uppercase inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <x-ri-instagram-line class="-ml-0.5 size-5"/>
+                Instagram
             </button>
 
         </div>
@@ -34,43 +32,59 @@
             <div class="mx-auto max-w-7xl px-6 lg:px-8">
                 <div class="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2">
                     <div class="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10">
-                        <div class="bg-white py-16 sm:py-24 lg:py-32">
+                        <div class="bg-white py-4">
                             <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                                <h3 class="max-w-2xl text-center text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">Newsletter</h3>
-                                <p class="mt-6 text-center text-base/7 text-gray-600">Explicabo quo fugit vel facere ullam corrupti non dolores. Expedita eius sit sequi.</p>
+                                <h3 class="uppercase max-w-2xl text-center text-balance text-3xl font-semibold tracking-tight text-gray-900">
+                                    Newsletter
+                                </h3>
+                                <p class="mt-6 text-center text-base/7 text-gray-600">
+                                    Explicabo quo fugit vel facere ullam corrupti non dolores. Expedita eius sit sequi.
+                                </p>
                                 <form class="mt-10 max-w-md">
                                     <div class="flex gap-x-4">
                                         <label for="email-address" class="sr-only">Email address</label>
-                                        <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Enter your email">
-                                        <button type="submit" class="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Subscribe</button>
+                                        <input id="email-address" name="email" type="email" autocomplete="email"
+                                               required
+                                               class="min-w-0 flex-auto rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                               placeholder="Enter your email">
+                                        <button type="submit"
+                                                class="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                            Subscribe
+                                        </button>
                                     </div>
-                                    <p class="mt-4 text-sm/6 text-gray-900">We care about your data. Read our <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">privacy&nbsp;policy</a>.</p>
+                                    <p class="mt-10 text-center text-xs text-gray-600">
+                                        En validant ce formulaire, je consens à l'utilisation de mes données personnelles
+                                        pour être tenu·e au courant des actualités de Drama Queerz. Plus d'infos sur
+                                        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">
+                                            Politique&nbsp;de&nbsp;confidentialité
+                                        </a>
+                                    </p>
                                 </form>
                             </div>
                         </div>
                     </div>
                     <div class="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10">
-                        <div class="bg-white py-16 sm:py-24 lg:py-32">
+                        <div class="bg-white py-4">
                             <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                                <h3 class="max-w-2xl text-center text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+                                <h3 class="uppercase max-w-2xl text-center text-balance text-3xl font-semibold tracking-tight text-gray-900">
                                     Comment les rats sont morts
                                 </h3>
-                                <p class="mt-6 text-center text-base/7 text-gray-600">Explicabo quo fugit vel facere ullam corrupti non dolores. Expedita eius sit sequi.</p>
+                                <p class="mt-6 text-center text-base/7 text-gray-600">Explicabo quo fugit vel facere
+                                    ullam corrupti non dolores. Expedita eius sit sequi.</p>
                                 <div class="flex gap-x-4 mt-10 justify-center">
-                                    <button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    <button type="button"
+                                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                         Répondre au formulaire
                                     </button>
                                 </div>
                                 <div class="flex gap-x-4 mt-6 justify-center">
-                                    <button type="button" class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        <svg class="-ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                            <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" />
-                                        </svg>
+                                    <button type="button"
+                                            class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                        <x-ri-bluesky-fill class="-ml-0.5 size-5"/>
                                     </button>
-                                    <button type="button" class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        <svg class="-ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                            <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" />
-                                        </svg>
+                                    <button type="button"
+                                            class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                        <x-ri-instagram-line class="-ml-0.5 size-5"/>
                                     </button>
                                 </div>
                             </div>
